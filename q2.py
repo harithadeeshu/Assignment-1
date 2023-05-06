cdc7b75b7a2216f1be4bb4b281716696cd1f4a41
harithadeeshu <131469436+harithadeeshu@users.noreply.github.com>
2023-05-06 15:28:29 +0530

    Add files via upload

---
+++ q2.py
@@ -0,0 +1,12 @@
+#Write a Python program that accepts a word from the user and reverse it.
+#Sample Test Case
+#Input : Edyoda
+#output: adoydE
+
+
+name=(input("enter your name  ",))
+str=''
+for i in name:
+    str=i+str 
+print("enter your name in reverse ",str) 
+
